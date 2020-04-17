458b6c9706fe29be8ea84a493a442b7f48bc1eca
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-04-17 17:53:37 +0800

    feat(isref/torefs): isRef、toRefs的用法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="hello">
     HelloWord组件的object：{{object.user}}
+    <!-- isRef: 判断一个变量是否是一个ref类型  -->
+
+    <!-- toRefs:由于reactive初始化的数据没有get、set方法，所有需要转成ref类型 -->
+
+    
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { ref, reactive, isRef, toRefs } from 'vue'
 export default {
   name: 'HelloWorld',
   props: {
@@ -15,6 +20,12 @@ export default {
     const object = reactive({ 
       user: 'zhangsan' 
     })
+    const countNum = ref(0)
+    const unwrapped = isRef(countNum) ? countNum.value : countNum
+    // console.log(unwrapped)
+    console.log(object)
+    const stateAsRefs = toRefs(object)
+    console.log(stateAsRefs, '====')
 
     return {
       object
