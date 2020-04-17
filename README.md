8c1b577202a75adeb6100701975078c2ec26fa59
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-04-17 17:32:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# vue3
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Compiles and minifies for production
+```
+vue add vue-next
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{count}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const count = ref(0)
+
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
